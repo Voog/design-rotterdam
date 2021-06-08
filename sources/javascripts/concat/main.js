1f5575ce68cb078ce6c289d73dfe6d1b397c4d46
pxska <kristjanparnmets@gmail.com>
2021-06-08 11:03:28 +0300

    Move JS used in editmode to correct file

--- sources/javascripts/concat/main.js
+++ sources/javascripts/concat/main.js
@@ -64,52 +64,6 @@
     }
   };
 
-  var editmode = $('html').hasClass('editmode');
-
-  // Remove comments if debouncing is used.
-  // Function to limit the rate at which a function can fire.
-  // var debounce = function(func, wait, immediate) {
-  //   var timeout;
-  //   return function() {
-  //     var context = this, args = arguments;
-  //     var later = function() {
-  //       timeout = null;
-  //       if (!immediate) func.apply(context, args);
-  //     };
-  //     var callNow = immediate && !timeout;
-  //     clearTimeout(timeout);
-  //     timeout = setTimeout(later, wait);
-  //     if (callNow) func.apply(context, args);
-  //   };
-  // };
-
-  var toggleFlags = function() {
-    $('.js-option-toggle-flags').on('click', function(event) {
-      event.stopPropagation();
-
-      if ($(this).hasClass('js-flag-disable-btn')) {
-        var flagsState = false;
-      } else {
-        var flagsState = true;
-      }
-
-      $(this).toggleClass('js-flag-disable-btn');
-      $('body').toggleClass('flags-enabled flags-disabled');
-
-      siteData.set("flags_state", flagsState);
-    });
-  };
-
-  var bindFallbackHeaderLeftWidthCalculation = function() {
-    var headerWidth = $('.js-header-inner').width(),
-        headerRight = $('.js-header-right'),
-        headerRightWidth = headerRight.width(),
-        headerRightMargin = parseInt(headerRight.css('margin-left')) + 1;
-
-
-    $('.js-header-left').css('min-width', headerWidth - headerRightWidth - headerRightMargin);
-  };
-
   // TODO: Remove if Edicy is going to wrap table with the container
   var wrapTables = function() {
     if (!editmode) {
@@ -224,101 +178,6 @@
     } else {
       $('.js-background-type').addClass('dark-background').removeClass('light-background');
     }
-  };
-
-  // Header background image and color preview logic function.
-  var headerBgPreview = function(data, header) {
-    // Defines the variables used in preview logic.
-
-    var headerBgImagePrevious = $('.js-header-banner').css('background-image'),
-        headerBgImageSuitable = data.imageSizes ? getImageByWidth(data.imageSizes, $(window).width()) : null,
-        headerBgImage = (data.image && data.image !== '') ? 'url(' + headerBgImageSuitable.url + ')' : 'none',
-        headerBgImageSizes = (data.imageSizes && data.imageSizes !== '') ? data.imageSizes : null,
-        headerBgColor = (data.color && data.color !== '') ? data.color : 'rgba(0,0,0,0)',
-        headerBgColorDataLightness = (data.colorData && data.colorData !== '') ? data.colorData.lightness : 1,
-        colorExtractImage = $('<img>'),
-        colorExtractCanvas = $('<canvas>'),
-        colorExtractImageUrl = (data.image && data.image !== '') ? data.image : null;
-
-    if (colorExtractImageUrl) {
-      if (headerBgImageSizesContains(headerBgImageSizes, headerBgImagePrevious)) {
-        headerBg.headerBgImageColor = headerBg.headerBgImageColor ? headerBg.headerBgImageColor : 'rgba(178,198,207,1)';
-        headerBgCombinedLightness = getCombinedLightness(headerBg.headerBgImageColor, headerBgColor);
-        handleHeaderImageLightnessClass();
-      } else {
-        colorExtractImage.attr('src', colorExtractImageUrl.replace(/.*\/(photos|voogstock)/g,'/photos'));
-        colorExtractImage.on('load', function() {
-          ColorExtract.extract(colorExtractImage[0], colorExtractCanvas[0], function(data) {
-            headerBg.headerBgImageColor = data.bgColor ? data.bgColor : 'rgba(255,255,255,1)';
-            headerBgCombinedLightness = getCombinedLightness(headerBg.headerBgImageColor, headerBgColor);
-            handleHeaderImageLightnessClass();
-          });
-        });
-      };
-    } else {
-      headerBg.headerBgImageColor = 'rgba(255,255,255,1)';
-      headerBgCombinedLightness = getCombinedLightness(headerBg.headerBgImageColor, headerBgColor);
-      handleHeaderImageLightnessClass();
-    };
-
-    // Updates the header background image and background color.
-    $(header).css({'background-image' : headerBgImage});
-    $(header).find('.background-color').css({'background-color' : headerBgColor});
-  };
-
-  var normalizeValue = function(value) {
-    if (value == null || (typeof value == 'string' && value.match(/^[\\'"]+$/))) {
-      return '';
-    } else {
-      return value;
-    }
-  };
-
-  // Header background image and color save logic function.
-  var headerBgCommit = function(data, dataName) {
-    var commitData = $.extend(true, {}, data);
-    commitData.image = data.image || '';
-    commitData.imageSizes = normalizeValue(data.imageSizes);
-    commitData.color = data.color || 'rgba(255,255,255,0)';
-    commitData.combinedLightness = headerBgCombinedLightness;
-    pageData.set(dataName, commitData);
-  }
-
-  var colorSum = function(bgColor, fgColor) {
-    if (bgColor && fgColor) {
-      if (typeof bgColor == 'string') {
-        bgColor = bgColor.replace(/rgba?\(/,'').replace(/\)/,'').split(',');
-        $.each(bgColor, function(n, x) {bgColor[n] = +x;});
-      }
-      if (typeof fgColor == 'string') {
-        fgColor = fgColor.replace(/rgba?\(/,'').replace(/\)/,'').split(',');
-        $.each(fgColor, function(n, x) {fgColor[n] = +x;});
-      }
-      if (typeof bgColor == 'object' && bgColor.hasOwnProperty('length')) {
-        if (bgColor.length == 3) { bgColor.push(1.0); }
-      }
-      if (typeof fgColor == 'object' && fgColor.hasOwnProperty('length')) {
-        if (fgColor.length == 3) { fgColor.push(1.0); }
-      }
-      var result = [0, 0, 0, 0];
-      result[3] = 1 - (1 - fgColor[3]) * (1 - bgColor[3]);
-      if (result[3] === 0) { result[3] = 1e-6; }
-      result[0] = Math.min(fgColor[0] * fgColor[3] / result[3] + bgColor[0] * bgColor[3] * (1 - fgColor[3]) / result[3], 255);
-      result[1] = Math.min(fgColor[1] * fgColor[3] / result[3] + bgColor[1] * bgColor[3] * (1 - fgColor[3]) / result[3], 255);
-      result[2] = Math.min(fgColor[2] * fgColor[3] / result[3] + bgColor[2] * bgColor[3] * (1 - fgColor[3]) / result[3], 255);
-      return $.map(result, function(e) { return Math.floor(e); });
-    }
-  };
-
-  var getCombinedColor = function(bgColor, fgColor) {
-    var sum = colorSum(bgColor || [255,255,255,1], fgColor || [255,255,255,1]);
-    return sum;
-  };
-
-  var getCombinedLightness = function(bgColor, fgColor) {
-    var combinedColor = getCombinedColor(bgColor, fgColor);
-    var color = Math.round(((+combinedColor[0]) * 0.2126 + (+combinedColor[1]) * 0.7152 + (+combinedColor[2]) * 0.0722) / 2.55) / 100;
-    return color;
   };
 
   var handleHeaderColorScheme = function(lightness) {
@@ -622,11 +481,6 @@
   });
 };
 
-var bindCustomTexteditorStyles = function(buttonTranslation) {
-  window.edy = window.edy || [];
-  edy.push(['texteditorStyles', {name: buttonTranslation, tagname:'a', attribute: {'href': '#'}, classname: 'custom-btn', toggle: true}]);
-};
-
 var init = function() {
   toggleMainMenu();
   toggleLangMenu();
@@ -637,10 +491,6 @@ var init = function() {
   focusCommentsWithErrors();
 
   $('.content form').edicyFormPlaceholders();
-
-  if (!Modernizr.flexbox && editmode) {
-    bindFallbackHeaderLeftWidthCalculation();
-  };
 };
 
 // ===========================================================================
@@ -692,135 +542,6 @@ var init = function() {
           siteData.set('settings_root_item', data);
         }
       });
-    });
-  };
-
-// ===========================================================================
-  // Binds editmode image drop areas.
-  // ===========================================================================
-  var bindContentItemImgDropAreas = function(placeholderText) {
-    $('.js-content-item-img-drop-area').each(function(index, imgDropAreaTarget) {
-      var $imgDropAreaTarget = $(imgDropAreaTarget),
-          $contentItemBox = $imgDropAreaTarget.closest('.js-content-item-box'),
-          itemId = $contentItemBox.data('item-id'),
-          itemType = $contentItemBox.data('item-type'),
-          itemData = new Edicy.CustomData({
-            type: itemType,
-            id: itemId
-          });
-
-      var imgDropArea = new Edicy.ImgDropArea($imgDropAreaTarget, {
-        positionable: false,
-        target_width: 1280,
-        placeholder: '<div class="edy-img-drop-area-placeholder">' + placeholderText + '</div>',
-        removeBtn: '<div class="edy-img-drop-area-remove-image">' +
-                      '<div class="edy-img-drop-area-remove-image-ico">' +
-                        '<svg width="16" height="20" viewBox="0 0 26 30" xmlns="http://www.w3.org/2000/svg">' +
-                          '<g fill-rule="nonzero" fill="currentColor">' +
-                            '<g transform="translate(2 5)">' +
-                              '<path d="M0 .997h2V21c0 1 1 2 2 2h14c1 0 2-1 2-2V1h2v20c0 2.25-1.75 4-4 4H4c-2.25 0-4-1.75-4-4V.997z"/>' +
-                              '<rect x="10" y="4" width="2" height="16" rx="1"/>' +
-                              '<rect x="5" y="4" width="2" height="16" rx="1"/>' +
-                              '<rect x="15" y="4" width="2" height="16" rx="1"/>' +
-                            '</g>' +
-                            '<path d="M26 4v2H0V4h7V2c0-1 1-2 2-2h8c1 0 2 1 2 2v2h7zM9 4h8V3c0-.5-.5-1-1-1h-6c-.5 0-1 .5-1 1v1z"/>' +
-                          '</g>' +
-                        '</svg>' +
-                      '</div>' +
-                    '</div>',
-
-        change: function(data) {
-          var imageId;
-
-          $imgDropAreaTarget
-            .removeClass('is-cropped')
-            .addClass('not-cropped')
-            .css('opacity', .1)
-          ;
-
-          if (data) {
-            imageId = data.original_id;
-
-            $contentItemBox
-              .removeClass('without-image is-loaded with-error')
-              .addClass('with-image not-loaded')
-            ;
-
-            setImageOrientation($contentItemBox, data.width, data.height);
-          } else {
-            imageId = null;
-
-            $contentItemBox
-              .removeClass('with-image is-loaded with-error')
-              .addClass('without-image not-loaded')
-            ;
-
-            $contentItemBox.find('.edy-img-drop-area-placeholder').css('opacity', 0);
-          }
-
-          setItemImage($contentItemBox, $imgDropAreaTarget, itemId, imageId, itemType);
-        }
-      });
-    });
-  };
-
-  var setImageOrientation = function($contentItemBox, width, height) {
-    var $imgDropAreaTarget = $contentItemBox.find('.js-content-item-img-drop-area'),
-        $cropToggleButton = $contentItemBox.find('.js-toggle-crop-state');
-
-    if (width > height) {
-      $imgDropAreaTarget
-        .removeClass('image-landscape image-square image-portrait')
-        .addClass('image-landscape')
-      ;
-    } else if (width === height) {
-      $imgDropAreaTarget
-        .removeClass('image-landscape image-square image-portrait')
-        .addClass('image-square')
-      ;
-    } else {
-      $imgDropAreaTarget
-        .removeClass('image-landscape image-square image-portrait')
-        .addClass('image-portrait')
-      ;
-    }
-
-    if ($imgDropAreaTarget.hasClass('image-square')) {
-      $cropToggleButton
-        .removeClass('is-visible')
-        .addClass('is-hidden')
-      ;
-    } else {
-      $cropToggleButton
-        .removeClass('is-hidden')
-        .addClass('is-visible')
-      ;
-    }
-  };
-
-  var setItemImage = function($contentItemBox, $imgDropArea, itemId, imageId, itemType) {
-    var apiType = itemType === 'article' ? 'articles' : 'pages',
-        itemData = new Edicy.CustomData({
-          type: itemType,
-          id: itemId
-        });
-
-    $.ajax({
-       type: 'PATCH',
-       contentType: 'application/json',
-       url: '/admin/api/' + apiType +'/' + itemId,
-       data: JSON.stringify({'image_id': imageId}),
-       dataType: 'json',
-       success: function(data) {
-         itemData.set('image_crop_state', 'not-cropped');
-         $contentItemBox.removeClass('not-loaded with-error').addClass('is-loaded');
-         $contentItemBox.find('.edy-img-drop-area-placeholder').css('opacity', 1);
-         $imgDropArea.css('opacity', 1);
-       },
-       timeout: 30000,
-       error: function(data) {
-         $contentItemBox.removeClass('not-loaded is-loaded with-error').addClass('with-error');
-       }
     });
   };
 
@@ -890,13 +611,6 @@ var init = function() {
   };
 
   // ===========================================================================
-  // Function to detect if site is displayed in editmode.
-  // ===========================================================================
-  var editmode = function () {
-    return $('html').hasClass('editmode');
-  };
-
-  // ===========================================================================
   // Sets header menu initial width attribute for menu mode calculation.
   // ===========================================================================
   var setHeaderMenuInitialWidth = function() {
@@ -908,23 +622,18 @@ var init = function() {
   // Enables the usage of the initiations outside this file.
   // For example add "<script>site.initBlogPage();</script>" at the end of the "Blog & News" page to initiate blog listing view functions.
 
-window.site = $.extend(window.site || {}, {
-  initFrontPage: initFrontPage,
-  initCommonPage: initCommonPage,
-  initBlogPage: initBlogPage,
-  initArticlePage: initArticlePage,
-  toggleFlags: toggleFlags,
-  getImageByWidth: getImageByWidth,
-  headerBgPreview: headerBgPreview,
-  headerBgCommit: headerBgCommit,
-  initItemsPage: initItemsPage,
-  handleHeaderColorScheme: handleHeaderColorScheme,
-  bindCustomTexteditorStyles: bindCustomTexteditorStyles,
-  bindRootItemSettings: bindRootItemSettings,
-  bindContentItemImgDropAreas: bindContentItemImgDropAreas,
-  bindContentItemImageCropToggle: bindContentItemImageCropToggle,
-  bindSiteSearch: bindSiteSearch
-});
+  window.site = $.extend(window.site || {}, {
+    initFrontPage: initFrontPage,
+    initCommonPage: initCommonPage,
+    initBlogPage: initBlogPage,
+    initArticlePage: initArticlePage,
+    getImageByWidth: getImageByWidth,
+    initItemsPage: initItemsPage,
+    handleHeaderColorScheme: handleHeaderColorScheme,
+    bindRootItemSettings: bindRootItemSettings,
+    bindContentItemImageCropToggle: bindContentItemImageCropToggle,
+    bindSiteSearch: bindSiteSearch
+  });
 
-init();
+  init();
 })(jQuery);
